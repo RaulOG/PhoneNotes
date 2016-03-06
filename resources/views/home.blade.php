2e8cb6b6573fb5b49dda9da84f96d286513f2a84
RaulOG <raulog1989@gmail.com>
2016-03-06 22:32:30 +0100

    Add basic view files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    Phone calls app! Log in, please.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/phonecalls/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Overview')
+
+@section('content')
+    Hi this is the overview
+@endsection
