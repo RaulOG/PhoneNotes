68c33f4a2e0dcc46fbe211b0885d0d258c058423
RaulOG <raulog1989@gmail.com>
2016-03-07 23:33:47 +0100

    Overview changes on data display and responsive design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    @yield('css')
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
@@ -28,24 +29,23 @@
 <body id="app-layout">
     <nav class="navbar navbar-default">
         <div class="container">
+            <div class="navbar-header">
 
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Home
+                </a>
+            </div>
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Home
-                    </a>
-                </div>
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
@@ -74,6 +74,7 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
--- resources/views/phonecalls/index.blade.php
+++ resources/views/phonecalls/index.blade.php
@@ -2,39 +2,48 @@
 
 @section('title', 'Overview')
 
-@section('content')
+@section('css')
+    {{ Html::style('css/overview.css') }}
+@endsection
 
-    <div class="panel panel-default">
-        <div class="panel-heading">Phone calls</div>
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">
+                Phone calls
+            </h1>
+        </div>
 
         <!-- Table -->
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>Phone number</th>
-                <th>Notes</th>
-                <th>Date/time</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-            </thead>
-            <tbody>
-            @forelse($phoneCalls as $phoneCall)
+        <div id="no-more-tables">
+            <table class="col-md-12 table-bordered table-striped table-condensed cf">
+                <thead class="cf">
                 <tr>
-                    <td>{{ $phoneCall->name }}</td>
-                    <td>{{ $phoneCall->phone_number }}</td>
-                    <td>{{ $phoneCall->notes }}</td>
-                    <td>{{ $phoneCall->created_at }}</td>
-                    <td>{{ link_to_route('phone_calls.edit', 'Modify', $phoneCall->id) }}</td>
-                    <td>{!! delete_form(['phone_calls.destroy', $phoneCall->id], '<span class="glyphicon glyphicon-trash"></span>', ['class' => 'btn btn-xs btn-danger']) !!}</td>
+                    <th>Name</th>
+                    <th>Phone number</th>
+                    <th>Notes</th>
+                    <th>Date/time</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 </tr>
-            @empty
-                <tr>
-                    <td>No phone calls</td>
-                </tr>
-            @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @forelse($phoneCalls as $phoneCall)
+                    <tr>
+                        <td data-title="Name">{{ $phoneCall->name }}</td>
+                        <td data-title="Phone number">{{ $phoneCall->phone_number }}</td>
+                        <td data-title="Notes">{{ $phoneCall->notes }}</td>
+                        <td data-title="Created at">{{ $phoneCall->created_at }}</td>
+                        <td data-title="Edit">{!! modify_form(['phone_calls.edit', $phoneCall->id], '<span class="glyphicon glyphicon-pencil"></span>') !!}</td>
+                        <td data-title="Delete">{!! delete_form(['phone_calls.destroy', $phoneCall->id], '<span class="glyphicon glyphicon-trash"></span>') !!}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td>No phone calls</td>
+                    </tr>
+                @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
