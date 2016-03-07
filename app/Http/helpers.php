aee16b558a53a1fb637951644e8a8dc4b70bb435
RaulOG <raulog1989@gmail.com>
2016-03-07 23:32:42 +0100

    Modify helper for edit button in forms (used atm in overview)

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -6,10 +6,22 @@
  */
 function delete_form(array $routeParams, $label = 'Delete', array $options = [])
 {
-    $class = (isset($options['class'])) ? $options['class'] : 'btn btn-danger';
+    $class = (isset($options['class'])) ? $options['class'] : 'btn btn-xs btn-danger';
 
     $form = '';
     $form .= Form::open(['method' => 'DELETE', 'route' => $routeParams]);
+    $form .= Form::button($label, ['type' => 'submit', 'class' => $class]);
+    $form .= Form::close();
+
+    return $form;
+}
+
+function modify_form(array $routeParams, $label = 'Edit', array $options = [])
+{
+    $class = (isset($options['class'])) ? $options['class'] : 'btn btn-xs btn-warning';
+
+    $form = '';
+    $form .= Form::open(['method' => 'GET', 'route' => $routeParams]);
     $form .= Form::button($label, ['type' => 'submit', 'class' => $class]);
     $form .= Form::close();
 
